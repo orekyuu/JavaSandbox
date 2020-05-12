59c6fde04e4c7a4d0a0ddba864cd648ce99d68dd
orekyuu <orekyuu@gmail.com>
2020-05-12 20:31:56 +0900

    コレクションのバリデーション

--- spring-validation/src/test/java/org/orekyuu/spring/validation/ValidationSample.java
+++ spring-validation/src/test/java/org/orekyuu/spring/validation/ValidationSample.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Path;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.StreamSupport;
 
 public class ValidationSample {
     Validator validator;
@@ -58,5 +65,44 @@ public class ValidationSample {
                     .first().extracting(ConstraintViolation::getMessage).asString()
                     .isEqualTo("must be greater than or equal to 0");
         }
+    }
+
+    /**
+     * https://beanvalidation.org/2.0/spec/#valueextractordefinition-builtinvalueextractors
+     */
+    class DefaultExtractorForm {
+        List<@NotEmpty String> stringList;
+
+        Map<@NotEmpty String, @Max(100) @Min(0) Integer> map;
+
+        Optional<@NotEmpty String> optional;
+
+        public DefaultExtractorForm(List<@NotEmpty String> stringList, Map<@NotEmpty String, @Max(100) @Min(0) Integer> map, Optional<@NotEmpty String> optional) {
+            this.stringList = stringList;
+            this.map = map;
+            this.optional = optional;
+        }
+    }
+
+    @Test
+    void extractor() {
+        DefaultExtractorForm form = new DefaultExtractorForm(List.of("str", ""), Map.of("key", -1), Optional.of(""));
+        var validate = validator.validate(form);
+        var assertion = Assertions.assertThat(validate);
+
+        assertion.filteredOn(conditionFactory("stringList")).hasSize(1).first()
+                .extracting(ConstraintViolation::getMessage).isEqualTo("must not be empty");
+
+        assertion.filteredOn(conditionFactory("map")).hasSize(1).first()
+                .extracting(ConstraintViolation::getMessage).isEqualTo("must be greater than or equal to 0");
+
+        assertion.filteredOn(conditionFactory("optional")).hasSize(1).first()
+                .extracting(ConstraintViolation::getMessage).isEqualTo("must not be empty");
+    }
+
+    private Predicate<ConstraintViolation<?>> conditionFactory(String fieldName) {
+        return it -> StreamSupport.stream(it.getPropertyPath().spliterator(), false)
+                .map(Path.Node::getName)
+                .anyMatch(fieldName::equals);
     }
 }
